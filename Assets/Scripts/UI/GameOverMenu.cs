fe60fbd5ddbf4fe1ea32eace10eb702ddad61427
Alex Covert <acovert@ucsc.edu>
2019-02-13 17:52:49 -0800

    make controllers work with game over menus

--- Assets/Scripts/UI/GameOverMenu.cs
+++ Assets/Scripts/UI/GameOverMenu.cs
@@ -2,8 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class GameOverMenu : MonoBehaviour {
+    [SerializeField] private EventSystem es;
+    [SerializeField] private GameObject[] menuButtons;
+    private CheckControllers cc;
+
+    private void Start()
+    {
+        cc = GetComponent<CheckControllers>();
+        if(cc.GetControllerOneState())
+        {
+            es.SetSelectedGameObject(menuButtons[0]);
+        }
+    }
 
     public void onClickRetry()
     {
