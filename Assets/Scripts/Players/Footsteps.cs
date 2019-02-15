8127e9a5bae33b5f9e218d2ce48b6347838794b9
Greg Anthony Ocampo <glocampo@ucsc.edu>
2019-02-14 17:11:56 -0800

    Added Jump, Land, Running sfx, scripted to animation events, and lowered volume of bgm

---
+++ Assets/Scripts/Players/Footsteps.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Footsteps : MonoBehaviour {
+
+    [SerializeField]
+    private AudioClip[] clips;
+
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void RightFootstep()
+    {
+        AudioClip clip = GetRightClip();
+        audioSource.PlayOneShot(clip);
+    }
+
+    private void LeftFootstep()
+    {
+        AudioClip clip = GetLeftClip();
+        audioSource.PlayOneShot(clip);
+    }
+
+    private AudioClip GetRightClip()
+    {
+        return clips[0];
+    }
+
+    private AudioClip GetLeftClip()
+    {
+        return clips[1];
+    }
+}
