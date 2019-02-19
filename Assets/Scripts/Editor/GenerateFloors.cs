d8879b67cb2fabe959fd59517f31d689833db8c2
Alex Covert <acovert@ucsc.edu>
2019-02-19 13:44:22 -0800

    script to instantiate floors in editor, deleted previous tower from scene and updated references on other objs

---
+++ Assets/Scripts/Editor/GenerateFloors.cs
@@ -0,0 +1,35 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class GenerateFloors {
+    //Change these to generate a different # of floors.
+    private static int floorHeight = 20;
+    public static int NumFloors = 7;
+
+
+    [MenuItem("GameObject/Instantiate Floors", false, 11)]
+    private static void Create()
+    {
+        GameObject previouslyInstantiated = GameObject.Find("ProceduralFloorsPrefab");
+        if (previouslyInstantiated == null)
+        {
+            for (int floor = 0; floor < NumFloors; floor++)
+            {
+                GameObject newFloor = PrefabUtility.InstantiatePrefab(Resources.Load("ProceduralFloorsPrefab")) as GameObject;
+                newFloor.transform.position = new Vector3(0, floorHeight * floor, 0);
+                newFloor.transform.SetParent(GameObject.Find("Tower").transform);
+
+                if(floor == NumFloors - 1)
+                {
+                    GameObject winTrigger = PrefabUtility.InstantiatePrefab(Resources.Load("WinTrigger")) as GameObject;
+                    winTrigger.transform.position = new Vector3(winTrigger.transform.position.x, floorHeight * floor + 10, winTrigger.transform.position.z);
+                    winTrigger.transform.SetParent(GameObject.Find("Tower").transform);
+                }
+            }
+        }
+        else
+        {
+            Debug.Log("Delete previously instantiated floors & previously instantiated win trigger from Tower parent game object first.");
+        }
+    }
+}
