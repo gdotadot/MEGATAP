5e3c77b4575a47ffbf0595d0513ff82252a40b77
Alex Covert <acovert@ucsc.edu>
2019-02-20 16:37:01 -0800

    Create profile

---
+++ Assets/Scripts/Spells/BlurSpell.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class BlurSpell : MonoBehaviour {
+    [SerializeField] private float timeToBlur;
+
+    private Camera bottomCam;
+
+    private PostProcessVolume postProcess;
+    private DepthOfField blur = null;
+
+    private void Start()
+    {
+        bottomCam = GameObject.Find("Player 1 Camera").GetComponent<Camera>();
+        postProcess = bottomCam.GetComponent<PostProcessVolume>();
+        postProcess.profile.TryGetSettings(out blur);
+
+        blur.active = true;
+    }
+    
+    IEnumerator StopBlur()
+    {
+        yield return new WaitForSeconds(timeToBlur);
+        blur.active = false;
+    }
+}
