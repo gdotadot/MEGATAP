a86476f9fb4d6c8d854eb0fe48dbbafa25a213be
Trevor Jordy <tjordy@ucsc.edu>
2019-02-27 15:15:08 -0800

    added ability to cast from ceiling

---
+++ Assets/Scripts/Spells/Lightning.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lightning : MonoBehaviour
+{
+
+    private SpellBase spellBase;
+
+    [SerializeField] private float stunDuration;
+
+    // Use this for initialization
+    void Start()
+    {
+        spellBase = this.GetComponent<SpellBase>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("zzap");
+            Debug.Log(other.GetComponent<GameObject>());
+            spellBase.Stun(other.GetComponent<GameObject>(), stunDuration);
+        }
+    }
+}
