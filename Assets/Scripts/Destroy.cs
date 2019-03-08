0f4aeb2948bd33f8e1a71e98257378f3dbe8c83c
AndyLongUCSC <andlong@ucsc.edu>
2019-03-07 16:05:14 -0800

    Destroys on each face properly

--- Assets/Scripts/Destroy.cs
+++ Assets/Scripts/Destroy.cs
@@ -21,7 +21,7 @@ public class Destroy : MonoBehaviour {
 
     private void Update()
     {
-        if(bottomPlayer.GetFloor() >= floor || (bottomPlayer.GetFloor() == floor && bottomPlayer.GetState() > face))
+        if(bottomPlayer.GetFloor() > floor || (bottomPlayer.GetFloor() == floor && bottomPlayer.GetState() > face))
         {
             Component[] components = GetComponents<Component>();
             Component[] childComponents = GetComponentsInChildren<Component>();
