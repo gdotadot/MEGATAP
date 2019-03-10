d7084a896c02591f9404b6fb6101c45e7f89e459
Trevor Jordy <tjordy@ucsc.edu>
2019-03-09 21:33:36 -0800

    tutorial controller script functional

--- Assets/Scripts/UI/TutorialController.cs
+++ Assets/Scripts/UI/TutorialController.cs
@@ -11,19 +11,49 @@ public class TutorialController : MonoBehaviour {
     void Start () {
 		foreach( GameObject tip in p1tips)
         {
-            Debug.Log(tip.GetComponent<CanvasRenderer>().GetAlpha());
             tip.GetComponent<CanvasRenderer>().SetAlpha(0f);
+            tip.transform.GetChild(0).gameObject.GetComponent<CanvasRenderer>().SetAlpha(0f);
         }
-	}
+        foreach (GameObject tip in p2tips)
+        {
+            tip.GetComponent<CanvasRenderer>().SetAlpha(0f);
+            tip.transform.GetChild(0).gameObject.GetComponent<CanvasRenderer>().SetAlpha(0f);
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        // TOP PLAYER
+        // spawn a tip ever 10 seconds
+        if((int)Time.timeSinceLevelLoad % 10 == 0 && (int)Time.timeSinceLevelLoad/10 < p1tips.Count)
+        {
+            GameObject tip = p1tips[(int)Time.timeSinceLevelLoad/10];
+            tip.GetComponent<CanvasRenderer>().SetAlpha(1f);
+            tip.transform.GetChild(0).gameObject.GetComponent<CanvasRenderer>().SetAlpha(1f);
+        }
+        // remove the previous tip every 10 seconds
+        if ((int)Time.timeSinceLevelLoad % 10 == 0 && (int)Time.timeSinceLevelLoad / 10 > 0 && (int)Time.timeSinceLevelLoad / 10 < p1tips.Count + 2)
+        {
+            GameObject tip = p1tips[((int)Time.timeSinceLevelLoad / 10) -1];
+            tip.GetComponent<CanvasRenderer>().SetAlpha(0f);
+            tip.transform.GetChild(0).gameObject.GetComponent<CanvasRenderer>().SetAlpha(0f);
+        }
 
-
-    public static void FadeIn(GameObject g)
-    {
-        g.GetComponent<CanvasRenderer>().SetAlpha(0f);
+        // BOTTOM PLAYER
+        // spawn a tip ever 10 seconds
+        if ((int)Time.timeSinceLevelLoad % 10 == 0 && (int)Time.timeSinceLevelLoad / 10 < p2tips.Count)
+        {
+            GameObject tip = p2tips[(int)Time.timeSinceLevelLoad / 10];
+            tip.GetComponent<CanvasRenderer>().SetAlpha(1f);
+            tip.transform.GetChild(0).gameObject.GetComponent<CanvasRenderer>().SetAlpha(1f);
+        }
+        // remove the previous tip every 10 seconds
+        if ((int)Time.timeSinceLevelLoad % 10 == 0 && (int)Time.timeSinceLevelLoad / 10 > 0 && (int)Time.timeSinceLevelLoad / 10 < p2tips.Count+2)
+        {
+            GameObject tip = p2tips[((int)Time.timeSinceLevelLoad / 10) - 1];
+            tip.GetComponent<CanvasRenderer>().SetAlpha(0f);
+            tip.transform.GetChild(0).gameObject.GetComponent<CanvasRenderer>().SetAlpha(0f);
+        }
+    }
  
 }
