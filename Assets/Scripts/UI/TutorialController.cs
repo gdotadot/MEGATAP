26a13b49d9b86d8a0cac5b774c652b93b7ca0466
Trevor Jordy <tjordy@ucsc.edu>
2019-03-09 21:00:33 -0800

    started script to control tutorial tip spawns

--- Assets/Scripts/UI/TutorialController.cs
+++ Assets/Scripts/UI/TutorialController.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class TutorialController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    [SerializeField] private List<GameObject> p1tips;
+    [SerializeField] private List<GameObject> p2tips;
+
+    // Use this for initialization
+    void Start () {
+		foreach( GameObject tip in p1tips)
+        {
+            Debug.Log(tip.GetComponent<CanvasRenderer>().GetAlpha());
+            tip.GetComponent<CanvasRenderer>().SetAlpha(0f);
+        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+
+    public static void FadeIn(GameObject g)
+    {
+        g.GetComponent<CanvasRenderer>().SetAlpha(0f);
+ 
 }
