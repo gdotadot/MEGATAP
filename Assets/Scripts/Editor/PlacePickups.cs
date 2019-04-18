83672dfab175decf34e213f4a96422cf0e38974c
AndyLongUCSC <andlong@ucsc.edu>
2019-04-18 14:54:42 -0700

    Generate Pickups Start

---
+++ Assets/Scripts/Editor/PlacePickups.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+using UnityEngine.SceneManagement;
+
+public class PlacePickups : MonoBehaviour {
+    //Change to Use
+    private static int NumFloors = 5;
+    private static int NumPickUpsFloor = 2;
+
+    private static int state = 1;
+    private static int floor = 1;
+    private static bool loop = true;
+    private static int index = 0;
+
+    private static List<GameObject> Faces = new List<GameObject>();
+
+    [MenuItem("GameObject/Generate Pickups", false, 15)]
+    // Use this for initialization
+    void Create()
+    {
+        GameObject item = GameObject.Find("PickUp");
+        List<GameObject> faces = new List<GameObject>();
+        Scene scene = SceneManager.GetActiveScene();
+        scene.GetRootGameObjects(faces);
+
+    }
+
+}
