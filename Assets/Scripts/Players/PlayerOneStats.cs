9ad1e73c127d50bd7c44e0aa1703bb258362e463
Trevor Jordy <tjordy@ucsc.edu>
2019-04-18 15:57:56 -0700

    added script to track stats

---
+++ Assets/Scripts/Players/PlayerOneStats.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerOneStats : MonoBehaviour {
+
+    [SerializeField] private int pickupCount;
+
+	// Use this for initialization
+	void Start () {
+        pickupCount = 0;
+        Debug.Log(pickupCount);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
