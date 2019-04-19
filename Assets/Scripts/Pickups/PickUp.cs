af5c4e80ec8c16ed243aaacab0c841fe452906a2
Trevor Jordy <tjordy@ucsc.edu>
2019-04-19 15:02:41 -0700

    making transparent, in progress

--- Assets/Scripts/Pickups/PickUp.cs
+++ Assets/Scripts/Pickups/PickUp.cs
@@ -6,12 +6,24 @@ public class PickUp : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+        this.gameObject.GetComponent<Renderer>().
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (GameObject.FindWithTag("Player").GetComponent<PlayerOneStats>().pickupCount == 3)
+        {
+            Color currColor = this.gameObject.GetComponent<Renderer>().material.color;
+            currColor.a = 0.0f;
+            this.gameObject.GetComponent<Renderer>().material.color = currColor;
+            Debug.Log("transparent");
+        } else
+        {
+            Color currColor = this.gameObject.GetComponent<Renderer>().material.color;
+            currColor.a = 1f;
+            this.gameObject.GetComponent<Renderer>().material.color = currColor;
+        }
 	}
 
     void OnTriggerEnter(Collider other)
