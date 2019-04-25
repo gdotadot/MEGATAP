097e79d261e6b53fee573435d54ab8f1b3f61aed
Trevor Jordy <tjordy@ucsc.edu>
2019-04-24 17:29:22 -0700

    created custom shader for pickup

--- Assets/Scripts/Pickups/PickUp.cs
+++ Assets/Scripts/Pickups/PickUp.cs
@@ -12,12 +12,11 @@ public class PickUp : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        if (GameObject.FindWithTag("Player").GetComponent<PlayerOneStats>().pickupCount == 3)
+        if (GameObject.FindWithTag("Player").GetComponent<PlayerOneStats>().pickupCount >= 3)
         {
             Color currColor = this.gameObject.GetComponent<Renderer>().material.color;
             currColor.a = 0.0f;
             this.gameObject.GetComponent<Renderer>().material.color = currColor;
-            Debug.Log("transparent");
         } else
         {
             Color currColor = this.gameObject.GetComponent<Renderer>().material.color;
