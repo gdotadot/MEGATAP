d4a5d27c2147d4aafd42a079309952da1d6e5e0f
Trevor Jordy <tjordy@ucsc.edu>
2019-04-25 16:19:28 -0700

    placed in feather instead of placeholder cube

--- Assets/Scripts/Pickups/PickUp.cs
+++ Assets/Scripts/Pickups/PickUp.cs
@@ -19,15 +19,15 @@ public class PickUp : MonoBehaviour {
         if (GameObject.FindWithTag("Player").GetComponent<PlayerOneStats>().pickupCount == 3)
         {
             // turn object transparent
-            Color currColor = this.gameObject.GetComponent<Renderer>().material.color;
+            Color currColor = this.gameObject.transform.GetChild(0).GetChild(1).GetComponent<SkinnedMeshRenderer>().material.color;
             currColor.a = 0.0f;
-            this.gameObject.GetComponent<Renderer>().material.color = currColor;
+            this.gameObject.transform.GetChild(0).GetChild(1).GetComponent<SkinnedMeshRenderer>().material.color = currColor;
         } else
         {
             // make it no longer transparent
-            Color currColor = this.gameObject.GetComponent<Renderer>().material.color;
+            Color currColor = this.gameObject.transform.GetChild(0).GetChild(1).GetComponent<SkinnedMeshRenderer>().material.color;
             currColor.a = 1f;
-            this.gameObject.GetComponent<Renderer>().material.color = currColor;
+            this.gameObject.transform.GetChild(0).GetChild(1).GetComponent<SkinnedMeshRenderer>().material.color = currColor;
         }
 	}
     
@@ -37,7 +37,7 @@ public class PickUp : MonoBehaviour {
         {
             active = false;
             other.GetComponent<PlayerOneStats>().pickupCount++;
-            this.gameObject.GetComponent<MeshRenderer>().enabled = false;
+            this.gameObject.transform.GetChild(0).GetChild(1).GetComponent<SkinnedMeshRenderer>().enabled = false;
 
             // initiate speed up
             if (GameObject.FindWithTag("Player").GetComponent<PlayerOneStats>().pickupCount >= 3)
