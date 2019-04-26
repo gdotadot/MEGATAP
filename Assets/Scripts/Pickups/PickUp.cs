b97aa48701fe89da01341507d5c1255630cad9f7
Trevor Jordy <tjordy@ucsc.edu>
2019-04-25 17:04:11 -0700

    sound implemented

--- Assets/Scripts/Pickups/PickUp.cs
+++ Assets/Scripts/Pickups/PickUp.cs
@@ -6,12 +6,18 @@ public class PickUp : MonoBehaviour {
 
     [SerializeField] private float speedUpDuration = 7f;
     [SerializeField] private float speedUpMultiplier = 3f;
+    [SerializeField] private AudioClip pickUpSFX1;
+    [SerializeField] private AudioClip pickUpSFX2;
+    [SerializeField] private AudioClip pickUpSFX3;
+    [SerializeField] private AudioClip speedBoostSFX;
+
+    private AudioSource audioSource;
     // active correlates to whether or not the pickup is faded out or not
     private bool active = true;
 
 	// Use this for initialization
 	void Start () {
-
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -33,10 +39,24 @@ public class PickUp : MonoBehaviour {
     
     void OnTriggerEnter(Collider other)
     {
+
         if (other.tag == "Player" && active == true)
         {
             active = false;
             other.GetComponent<PlayerOneStats>().pickupCount++;
+
+            if (other.gameObject.GetComponent<PlayerOneStats>().pickupCount == 1)
+            {
+                audioSource.PlayOneShot(pickUpSFX1);
+            } else if (other.gameObject.GetComponent<PlayerOneStats>().pickupCount == 2)
+            {
+                audioSource.PlayOneShot(pickUpSFX2);
+            } else if (other.gameObject.GetComponent<PlayerOneStats>().pickupCount == 3)
+            {
+                audioSource.PlayOneShot(pickUpSFX3);
+                audioSource.PlayOneShot(speedBoostSFX);
+            }
+
             this.gameObject.transform.GetChild(0).GetChild(1).GetComponent<SkinnedMeshRenderer>().enabled = false;
 
             // initiate speed up
