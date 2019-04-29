7d828cc8cba664cd66f0b9083c7047add248cc8d
AndyLongUCSC <andlong@ucsc.edu>
2019-04-29 13:28:35 -0700

    Projectile script start

---
+++ Assets/Scripts/Traps/LogProjectile.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogProjectile : MonoBehaviour {
+    private TrapBase trapBase;
+
+    private bool hit = false;
+    private GameObject player = null;
+    private Renderer[] child;
+
+    // Use this for initialization
+    void Start () {
+        trapBase = GetComponent<TrapBase>();
+        child = this.GetComponentsInChildren<Renderer>();
+        hit = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
